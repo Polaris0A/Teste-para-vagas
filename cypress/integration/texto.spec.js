4e88b801ba43e40ee44f167c3596e4187b9cd14e
Polaris0A <93089274+Polaris0A@users.noreply.github.com>
2022-01-31 14:22:50 -0300

    Add files via upload

---
+++ cypress/integration/texto.spec.js
@@ -0,0 +1,8 @@
+// /// <reference types="cypress" />
+
+// describe('Verificando texto na tela de compras', () => {
+//      it('Text', () => {
+//         cy.visit('https://www.genera.com.br/')
+//         cy.get('body').should('contain', 'insira')
+//     })
+// })
